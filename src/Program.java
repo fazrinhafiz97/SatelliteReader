602e4ab20dfca8f98dbee2b6a95a802c0eb1790c
fazrinhafiz97 <muhdfazrinhafiz@gmail.com>
2021-12-15 23:58:19 +0800

    New changes

--- src/Program.java
+++ src/Program.java
@@ -15,42 +15,41 @@ import java.util.Date;
 
 public class Program {
 
-	//international space satelite station id
+	// international space satellite station id
 	private static final int ISS_ID = 25544;
-	//api url
+	// api url
 	private static final String API_URL = "https://api.wheretheiss.at/v1/satellites/" + ISS_ID
 			+ "/positions?timestamps=";
 
-	//1. to convert the given date and create the timestamps
-	//2. call the api with time stamps
-	//3. parse the json and show the results
-	
+	// 1. to convert the given date and create the timestamps
+	// 2. call the api with time stamps
+	// 3. parse the json and show the results
+
 	public static void main(String[] args) {
-		//read the date time stamp from command line
+		// read the date time stamp from command line
 		String datetime = args[0];
-		
-		//parse the date time and get the time stamps in the format for API
+
+		// parse the date time and get the time stamps in the format for API
 		String stamps = getTimeStamps(datetime);
-		
-		//if user enters date in wrong format. exit the application
-		if(stamps == null) {
+
+		// if user enters date in wrong format. exit the application
+		if (stamps == null) {
 			return;
 		}
 		String resultJson = null;
 		try {
-			//call the api and read the json
+			// call the api and read the json
 			resultJson = getJson(stamps);
-			
-			//parse the json and show the results
-			if(resultJson != null) {
+
+			// parse the json and show the results
+			if (resultJson != null) {
 				parseJson(resultJson);
-				
-			}
-			else {
-				//if the api returns results not as expected
+
+			} else {
+				// if the api returns results not as expected
 				System.err.println("Error getting data");
 			}
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -58,51 +57,51 @@ public class Program {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	private static String getTimeStamps(String datetime) {
 		try {
-			//convert the user entered date in a format
+			// convert the user entered date in a format
 			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy hh:mmaa");
-			//convert string to date
+			// convert string to date
 			Date date = formatter.parse(datetime);
 			String timestamp = "";
-			
-			//run the loop so that we have time 
-			//1 hour before and 1 hour after the time specified
-			//where time interval between each date is 10 minutes
-			for(int i = 60; i >= -60; i = i - 10) {
+
+			// run the loop so that we have time
+			// 1 hour before and 1 hour after the time specified
+			// where time interval between each date is 10 minutes
+			for (int i = 60; i >= -60; i = i - 10) {
 				Calendar calendar = Calendar.getInstance();
 				calendar.setTime(date);
 				calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE) - i);
-				timestamp += calendar.getTimeInMillis()+",";
-				
+				timestamp += calendar.getTimeInMillis() + ",";
+
 			}
-			
-			//return the time stamp with comma removed at the end
-			return timestamp.substring(0, timestamp.length()-1);
-		}
-		catch(Exception e) {
+
+			// return the time stamp with comma removed at the end
+			return timestamp.substring(0, timestamp.length() - 1);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//return null, if the date is not formatted correctly
+		// return null, if the date is not formatted correctly
 		return null;
 	}
 
 	private static String getJson(String datetime) throws IOException, InterruptedException {
-		
-		//create the api by inserting the date time stamps in it
+
+		// create the api by inserting the date time stamps in it
 		String dte = API_URL + datetime + "&units=miles";
 		System.out.println(dte);
-		//create a http request
+		// create a http request
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(dte)).build();
-		//call the request
+		// call the request
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-		
-		//if api returns ok result
-		if(response.statusCode() == HttpsURLConnection.HTTP_OK) {
-			//return the json result
+
+		// if api returns ok result
+		if (response.statusCode() == HttpsURLConnection.HTTP_OK) {
+			// return the json result
 			return response.body();
 		}
 
@@ -110,14 +109,15 @@ public class Program {
 	}
 
 	private static void parseJson(String resultJson) {
-		//this will parse json
+		// this will parse json
 		Gson gson = new Gson();
-		//convert the json into the list of Satelite objects
-		ArrayList<Satelite> list = gson.fromJson(resultJson, new TypeToken<ArrayList<Satelite>>() {}.getType());
-		
-		//iterate over the list and show the string representation of each object
-		for(Satelite satelite : list) {
-			System.out.println(satelite);
+		// convert the json into the list of Satelite objects
+		ArrayList<Satellite> list = gson.fromJson(resultJson, new TypeToken<ArrayList<Satellite>>() {
+		}.getType());
+
+		// iterate over the list and show the string representation of each object
+		for (Satellite satellite : list) {
+			System.out.println(satellite);
 		}
 	}
 
--- src/Satellite.java
+++ src/Satellite.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-public class Satelite {
+public class Satellite {
 	private String name;
 	private int id;
 	private double latitude;
@@ -14,89 +14,115 @@ public class Satelite {
 	private double solar_lat;
 	private double solar_lon;
 	private String unit;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public double getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
+
 	public double getLongitude() {
 		return longitude;
 	}
+
 	public void setLongitude(double longitude) {
 		this.longitude = longitude;
 	}
+
 	public double getAltitude() {
 		return altitude;
 	}
+
 	public void setAltitude(double altitude) {
 		this.altitude = altitude;
 	}
+
 	public double getVelocity() {
 		return velocity;
 	}
+
 	public void setVelocity(double velocity) {
 		this.velocity = velocity;
 	}
+
 	public String getVisiblity() {
 		return visiblity;
 	}
+
 	public void setVisiblity(String visiblity) {
 		this.visiblity = visiblity;
 	}
+
 	public String getFootprint() {
 		return footprint;
 	}
+
 	public void setFootprint(String footprint) {
 		this.footprint = footprint;
 	}
+
 	public long getTimestamp() {
 		return timestamp;
 	}
+
 	public void setTimestamp(long timestamp) {
 		this.timestamp = timestamp;
 	}
+
 	public double getDaynum() {
 		return daynum;
 	}
+
 	public void setDaynum(double daynum) {
 		this.daynum = daynum;
 	}
+
 	public double getSolar_lat() {
 		return solar_lat;
 	}
+
 	public void setSolar_lat(double solar_lat) {
 		this.solar_lat = solar_lat;
 	}
+
 	public double getSolar_lon() {
 		return solar_lon;
 	}
+
 	public void setSolar_lon(double solar_lon) {
 		this.solar_lon = solar_lon;
 	}
+
 	public String getUnit() {
 		return unit;
 	}
+
 	public void setUnit(String unit) {
 		this.unit = unit;
 	}
-	
+
 	@Override
 	public String toString() {
-		//represents the string representation of this class
-		return new Date(timestamp).toString()+", Location (" + this.latitude + "," + this.longitude+")";
+		// represents the string representation of this class
+		return new Date(timestamp).toString() + ", Location (" + this.latitude + "," + this.longitude + ")";
 	}
 
 }
